cdba63d91a0f1de81517e7fa9f2a7279bb3211f4
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-02-27 15:10:38 +0900

    first commit

---
+++ src/main/java/com/example9/Tamo.java
@@ -0,0 +1,47 @@
+package com.example9;
+
+public class Tamo {
+
+	private String name;
+	private int age;
+	private String color;
+	private String gender;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "Tamo [name=" + name + ", age=" + age + ", color=" + color + ", gender=" + gender + "]";
+	}
+
+}
---
+++ src/main/java/com/example9/TestExApplication.java
@@ -0,0 +1,13 @@
+package com.example9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestExApplication.class, args);
+	}
+
+}
